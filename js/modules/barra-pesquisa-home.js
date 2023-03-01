f07e6d30ed4747a3badc940a7ff6e73f05cf2aa7
Bernardo Costa <bernardo-v.f@hotmail.com>
2023-03-01 07:42:31 -0300

    barra de pesquisa

--- js/modules/barra-pesquisa-home.js
+++ js/modules/barra-pesquisa-home.js
@@ -1,19 +1,30 @@
 export default function barraPesquisaHome(){
   const itemBarra = document.querySelectorAll('.item-click')
+  
+  
+  function navegarBarra(index){
+    const small = window.matchMedia('(max-width: 768px)');
+      itemBarra.forEach((item)=>{
+        const bolaAtual = item.querySelector('.bola')
+        const pAtual = item.querySelector('p')
+        bolaAtual.classList.remove('ativo')
+        pAtual.classList.remove('ativo')
+      })
+      const bolaAtiva = document.querySelectorAll('.item-click .bola')
+      const pAtivo = document.querySelectorAll('.item-click p')
+      bolaAtiva[index].classList.add('ativo')
+      pAtivo[index].classList.add('ativo')
+      barra(index)
+  }
+  
+  function barra(index){
   const progresso = document.querySelector('.progresso')
-
-
-function navegarBarra(index){
-  itemBarra.forEach((item)=>{
-    const bolaAtual = item.querySelector('.bola')
-    bolaAtual.classList.remove('ativo')
-  })
-  const bolaAtiva = document.querySelectorAll('.item-click .bola')
-  bolaAtiva[index].classList.add('ativo')
-  barra(index)
-}
-
-function barra(index){
+  if(index <= 0){
+    progresso.style.left = 0
+  }
+  let progressoCont = 100 - (25 * index)
+  console.log(progresso.style.left);
+  progresso.style.left = `calc(100% - ${progressoCont}%)`
 
 }
 
