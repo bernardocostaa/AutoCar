e570076e7b981f59e30323308142ee19a3bcb5ab
Bernardo Costa <bernardo-v.f@hotmail.com>
2023-03-03 22:56:36 -0300

    add imgs

--- js/modules/produtosCarros.js
+++ js/modules/produtosCarros.js
@@ -1,7 +1,31 @@
 export default async function produtosCarros(){
+    const sectionCarros = document.querySelectorAll('.coluna-carro')
     const produtoCarro = await fetch('./carros-produto.json')
     const json = await produtoCarro.json();
-    console.log(json[0].imgHome);
-  
+    let cardsCarros = ''
+    json.forEach((item)=>{
+        cardsCarros += `
+        <div class="carro">
+        <img class="img-carro img-response" src="${item.imgHome}" alt="">
+        <div>
+        <div class="info-carro">
+        <div class="valor-potencia mb-8">
+        <p class="cor-0 font-24-bold"><span class="cor-p1">RS</span> ${item.preco}</p>
+        <div class="cavalos">
+        <img src="imgs/icons/cv.svg" alt="">
+        <span class="font-p-roboto-16 cor-3">${item.cv} CV</span>
+        </div>
+        </div>
+        <p class="font-p-roboto-16 cor-5 mb-24">${item.nomeCar} </p>
+        <a class="btn-primary-carro font-btn" href="produto.html">COMPRE O SEU<img src="imgs/icons/carbtn.svg" alt=""></a>
+        </div>
+        </div>
+        </div>`;
+    })
+    sectionCarros.forEach((item,i)=>{
+        item.innerHTML = cardsCarros
+        console.log(item);
+    })
+    
 }
 
