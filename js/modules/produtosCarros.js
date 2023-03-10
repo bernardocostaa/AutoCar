56b9eeaca4b5500a44fafb2ac86c4d95de6661b9
Bernardo Costa <bernardo-v.f@hotmail.com>
2023-03-10 15:09:04 -0300

    ajuste json

--- js/modules/produtosCarros.js
+++ js/modules/produtosCarros.js
@@ -11,22 +11,21 @@ export default async function produtosCarros(){
             }
             coluna = 0
             filtro.forEach((item)=>{
-                console.log(item.home[0]);
                  if(coluna > sectionCarros.length - 1){
                      coluna = 0
                  }
                 sectionCarros[coluna].innerHTML += `<div class="carro">
-                <img class="img-carro img-response" src="${item.imgHome}" alt="">
+                <img class="img-carro img-response" src="${item.home[0].imgHome}" alt="">
                 <div>
                 <div class="info-carro">
                 <div class="valor-potencia mb-8">
-                <p class="cor-0 font-24-bold"><span class="cor-p1">RS</span> ${item.preco}</p>
+                <p class="cor-0 font-24-bold"><span class="cor-p1">RS</span> ${item.home[0].preco}</p>
                 <div class="cavalos">
                 <img src="imgs/icons/cv.svg" alt="">
-                <span class="font-p-roboto-16 cor-3">${item.cv} CV</span>
+                <span class="font-p-roboto-16 cor-3">${item.home[0].cv} CV</span>
                 </div>
                 </div>
-                <p class="font-p-roboto-16 cor-5 mb-24">${item.nomeCar} </p>
+                <p class="font-p-roboto-16 cor-5 mb-24">${item.home[0].nomeCar} </p>
                 <a class="btn-primary-carro font-btn" href="produto.html">COMPRE O SEU<img src="imgs/icons/carbtn.svg" alt=""></a>
                 </div>
                 </div>
@@ -37,8 +36,8 @@ export default async function produtosCarros(){
         function filtrarCarros(e){
             const clique = e.currentTarget.innerText
             const categoriaJson = json.filter((item)=>{
-                if(clique === item.categoria){
-                   return item.categoria
+                if(clique === item.home[0].categoria){
+                   return item.home[0].categoria
                 }
             })
             if(clique === 'Todos'){
